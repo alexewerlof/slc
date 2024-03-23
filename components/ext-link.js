2fabbba1c647113daede2939c9df9865eb91df85
Alex Ewerlöf <hanifbbz@gmail.com>
2024-03-23 10:58:44 +0100

    refactor ext-link to use external template

--- components/ext-link.js
+++ components/ext-link.js
@@ -1,11 +1,13 @@
+import { fetchTemplate } from '../lib/fetch-template.js'
+
 /* 
 This is just a convenience component to
 shorten the code for external links and
 reduce the risk of typo errors.
 */
 export default {
+    template: await fetchTemplate('ext-link.html', import.meta.url),
     props: {
         href: String,
     },
-    template: '<a :href="href" target="_blank" rel="noopener"><slot></slot></a>'
 }
