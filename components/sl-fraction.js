687d21d0e5976adec4a35b7f1ca45509d9ad75c2
Alex Ewerlöf <hanifbbz@gmail.com>
2024-08-11 10:30:42 +0200

    simplify SL formula component

--- components/sl-fraction.js
+++ components/sl-fraction.js
@@ -1,7 +1,5 @@
 import { loadComponent } from '../lib/fetch-template.js'
-import { humanSec } from "../lib/time.js"
 import { entity2symbolNorm } from '../lib/fmt.js'
-import { isNum } from '../lib/validation.js'
 
 /* 
 This is just a convenience component to
@@ -14,19 +12,16 @@ export default {
     template: await loadComponent(import.meta.url, true),
     props: {
         unit: String,
-        isTimeBased: Boolean,
         lowerBound: String,
         lowerThreshold: String,
         good: String,
         upperBound: String,
         upperThreshold: String,
         valid: String,
-        windowSec: Number,
+        timePeriod: String,
         timeLabelId: String,
     },
     methods: {
-        humanSec,
         entity2symbolNorm,
-        isNum,
     },
 }
