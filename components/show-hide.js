986670933cec0f79f0c11f105bdabd117d774a82
Alex Ewerlöf <hanifbbz@gmail.com>
2024-08-28 15:58:17 +0200

    create a component to show/hide content

---
+++ components/show-hide.js
@@ -0,0 +1,26 @@
+import { loadComponent } from '../lib/fetch-template.js'
+
+export default {
+    template: await loadComponent(import.meta.url),
+    data() {
+        return {
+            isVisible: true,
+        }
+    },
+    computed: {
+        icon() {
+            return this.isVisible ? '▼' : '▶'
+        },
+    },
+    props: {
+        title: {
+            type: String,
+            default: 'NO TITLE!',
+        },
+    },
+    methods: {
+        toggle() {
+            this.isVisible = !this.isVisible
+        },
+    },
+}
