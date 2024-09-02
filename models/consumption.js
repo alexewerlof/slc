8039a38ccc118ae0ba1a77c5ca10722000722b86
Alex Ewerlöf <hanifbbz@gmail.com>
2024-09-02 14:09:22 +0200

    add a guard for accidentally adding a service that does not have the correct assessment

--- models/consumption.js
+++ models/consumption.js
@@ -1,6 +1,7 @@
 import { namify } from '../lib/fmt.js'
 import { isInstance } from '../lib/validation.js'
 import { Consumer } from './consumer.js'
+import { Service } from './service.js'
 
 export class Consumption {
     constructor(consumer, title = '', description = '') {
@@ -22,6 +23,9 @@ export class Consumption {
             throw new Error(`Consumption.addDependency: service must be an instance of Service. Got ${service}`)
         }
         if (!this.hasDependency(service)) {
+            if (service.system.assessment !== this.consumer.assessment) {
+                throw new Error(`Consumption.addDependency: service ${service} is not in the same assessment as ${this.consumer}`)
+            }
             this.dependencies.push(service)
         }
     }
