3fe1cc26e552b1330a97e11efb5f289057c1fdf9
Alex Ewerlöf <hanifbbz@gmail.com>
2024-09-03 22:20:35 +0200

    remove dead code from Service

--- models/service.js
+++ models/service.js
@@ -1,25 +1,3 @@
-/*
-import * as yaml  from '../vendor/js-yaml.js'
-console.log(yaml)
-
-export class ServiceLevel {
-    constructor(title, description) {
-        this.title = title
-        this.description = description
-        this.indicator = []
-    }
-
-    get oslo() {
-
-    }
-
-    get osloYaml() {
-        return yaml.dump(this.oslo)
-    }
-}
-*/
-
-import { Level } from './level.js'
 import { isInstance } from '../lib/validation.js'
 import { System } from './system.js'
 
