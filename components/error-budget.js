8cc3d3f895a831a494a964d1560271d580aef10b
Alex Ewerlöf <hanifbbz@gmail.com>
2025-03-16 09:57:14 +0100

    move more of the text to the error budget component for reuse

--- components/error-budget.js
+++ components/error-budget.js
@@ -1,9 +1,15 @@
 import { loadComponent } from '../lib/fetch-template.js'
-import { numL10n } from '../lib/fmt.js'
+import { numL10n, percL10n } from '../lib/fmt.js'
 import { isObj } from '../lib/validation.js'
+import HelpComponent from '../components/help.js'
+import ExtLink from '../components/ext-link.js'
 
 export default {
     template: await loadComponent(import.meta.url),
+    components: {
+        HelpComponent,
+        ExtLink,
+    },
     computed: {
         errorBudgetGridTemplateColumns() {
             return {
@@ -13,6 +19,7 @@ export default {
     },
     methods: {
         numL10n,
+        percL10n,
     },
     props: {
         objective: {
