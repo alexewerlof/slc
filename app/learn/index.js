824bd8f0672a78b4c7ea42b84c3a463ce9d370e4
Alex Ewerlöf <hanifbbz@gmail.com>
2025-03-21 21:41:28 +0100

    very basic /learn page

---
+++ app/learn/index.js
@@ -0,0 +1,29 @@
+import { createApp } from '../../vendor/vue.js'
+import ExtLink from '../../components/ext-link.js'
+import FeedbackBlobComponent from '../../components/feedback-blob.js'
+import HeaderComponent from '../../components/header.js'
+import FooterComponent from '../../components/footer.js'
+import CookiePopupComponent from '../../components/cookie-popup.js'
+import HelpComponent from '../../components/help.js'
+import CalculatorViewComponent from '../../views/calculator-view.js'
+import CodeBlockComponent from '../../components/code-block.js'
+import FaqComponent from '../../components/faq.js'
+
+export const app = createApp({
+    data() {
+        return {}
+    },
+    components: {
+        CalculatorViewComponent,
+        CookiePopupComponent,
+        ExtLink,
+        FaqComponent,
+        FeedbackBlobComponent,
+        FooterComponent,
+        HeaderComponent,
+        HelpComponent,
+        CodeBlockComponent,
+    },
+})
+
+app.mount('#app')
---
+++ components/faq.js
@@ -0,0 +1,22 @@
+import { loadComponent } from '../lib/fetch-template.js'
+
+export default {
+    template: await loadComponent(import.meta.url),
+    data() {
+        return {
+            isVisible: this.visible,
+        }
+    },
+    props: {
+        question: {
+            type: String,
+            default: 'MISSING QUESTION!',
+        },
+        visible: Boolean,
+    },
+    methods: {
+        toggle() {
+            this.isVisible = !this.isVisible
+        },
+    },
+}
