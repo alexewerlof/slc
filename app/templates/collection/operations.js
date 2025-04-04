ffe76220d2dd2a1a904300d658db671eca4398b9
Alex Ewerlöf <hanifbbz@gmail.com>
2025-04-04 19:56:32 +0200

    add a couple of templates to operational services

--- app/templates/collection/operations.js
+++ app/templates/collection/operations.js
@@ -2,7 +2,7 @@ const commonTags = ['ops', 'operations']
 
 export default [
     {
-        title: 'Incident Handling Speed',
+        title: 'Latency: Incident Handling Speed',
         description: 'The percentage of highly severe incidents that were resolved sufficiently fast.',
         eventUnit: 'SEV1|2 incidents',
         metricName: 'time_to_restore',
@@ -12,7 +12,7 @@ export default [
         tags: [...commonTags, 'MTTR'],
     },
     {
-        title: 'Time To Acknowledge',
+        title: 'Latency: Time To Acknowledge',
         description: 'The percentage of high priority incidents that were acknowledged sufficiently fast.',
         eventUnit: 'SEV1 incidents',
         metricName: 'time_to_acknowledge',
@@ -20,5 +20,30 @@ export default [
         upperBound: 'le',
         upperThreshold: 5,
         tags: [...commonTags, 'MTTA'],
+    },
+    {
+        title: 'Accuracy: Incident Triage',
+        description: 'The percentage of incidents that were triaged in time.',
+        eventUnit: 'incidents',
+        metricName: 'time_to_triage',
+        metricUnit: 'min',
+        upperBound: 'le',
+        upperThreshold: 5,
+        tags: [...commonTags, 'MTTT'],
+    },
+    {
+        title: 'Efficiency: Automatic detection',
+        description: 'The percentage of incidents that were detected automatically.',
+        eventUnit: 'incidents',
+        metricName: 'detection_method == AUTO',
+        metricUnit: 'Boolean',
+    },
+    {
+        title: 'Accuracy: Non-false alerts',
+        description: 'The percentage of incidents that were not false alerts and were actually worth investigating.',
+        eventUnit: 'incidents',
+        metricName: 'alert_seveirty_level',
+        upperBound: 'le',
+        upperThreshold: 4,
     },
 ]
