3b527e97a4dbf832ac423335ea0953f5f57cb541
Alex Ewerlöf <hanifbbz@gmail.com>
2025-04-11 18:25:07 +0200

    now we can finally add indicators from templates

--- components/calculator-component.js
+++ components/calculator-component.js
@@ -4,8 +4,14 @@ import { icon } from '../lib/icons.js'
 import { Calculator } from './calculator.js'
 import { percentToRatio } from '../lib/math.js'
 import { isInstance } from '../lib/validation.js'
+import { Indicator } from './indicator.js'
 
 export default {
+    data() {
+        return {
+            isIndicatorTemplatesVisible: false,
+        }
+    },
     props: {
         calculator: {
             type: Object,
@@ -25,5 +31,14 @@ export default {
         numL10n,
         percentToRatio,
         percL10n,
+        addIndicatorFromSelector(indicator) {
+            if (!isInstance(indicator, Indicator)) {
+                throw new TypeError(`Expected an instance of Indicator. Got ${indicator}`)
+            }
+            this.isIndicatorTemplatesVisible = false
+            const options = indicator.save()
+            console.log('Adding indicator from selector:', options)
+            return this.calculator.addIndicator(new Indicator(options))
+        },
     },
 }
