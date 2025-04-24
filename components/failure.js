43e8fffd5c042555311c4632d54e7f1f29b6a17e
Alex Ewerlöf <hanifbbz@gmail.com>
2025-04-24 15:21:22 +0200

    modernize Failure state management

--- components/failure.js
+++ components/failure.js
@@ -1,34 +1,86 @@
-import { isInstance } from '../lib/validation.js'
+import { inRange, isDef, isInstance, isStr } from '../lib/validation.js'
 import { config } from '../config.js'
 import { Service } from './service.js'
 import { Consumption } from './consumption.js'
 import { icon } from '../lib/icons.js'
-import { clamp } from '../lib/math.js'
 
 const failureIcon = icon('failure')
 
 // If a certain service fails, what activities will it impact and how?
 export class Failure {
+    service = null
+    consumption = null
+    symptom = ''
+    consequence = ''
+    businessImpact = ''
+    impactLevel = config.impactLevel.default
+
     constructor(
         service,
-        consumption,
-        symptom = '',
-        consequence = '',
-        businessImpact = '',
-        impactLevel = config.impactLevel.default,
+        state,
     ) {
         if (!isInstance(service, Service)) {
             throw new Error(`Expected a Service instance. Got ${service}`)
         }
         this.service = service
-        if (!isInstance(consumption, Consumption)) {
-            throw new Error(`Expected a Consumption instance. Got ${consumption}`)
+        if (isDef(state)) {
+            this.state = state
         }
-        this.consumption = consumption
-        this.symptom = symptom
-        this.consequence = consequence
-        this.businessImpact = businessImpact
-        this.impactLevel = clamp(impactLevel, config.impactLevel.min, config.impactLevel.max)
+    }
+
+    set state(newState) {
+        if (!isObj(newState)) {
+            throw new TypeError(`state should be an object. Got: ${newState} (${typeof newState})`)
+        }
+
+        const {
+            consumption,
+            symptom,
+            consequence,
+            businessImpact,
+            impactLevel,
+        } = newState
+
+        if (isDef(consumption)) {
+            if (!isInstance(consumption, Consumption)) {
+                throw new TypeError(
+                    `consumption should be an instance of Consumption. Got: ${consumption} (${typeof consumption})`,
+                )
+            }
+            this.consumption = consumption
+        }
+
+        if (isDef(symptom)) {
+            if (!isStr(symptom)) {
+                throw new TypeError(`symptom should be a string. Got: ${symptom} (${typeof symptom})`)
+            }
+            this.symptom = symptom
+        }
+
+        if (isDef(consequence)) {
+            if (!isStr(consequence)) {
+                throw new TypeError(`consequence should be a string. Got: ${consequence} (${typeof consequence})`)
+            }
+            this.consequence = consequence
+        }
+
+        if (isDef(businessImpact)) {
+            if (!isStr(businessImpact)) {
+                throw new TypeError(
+                    `businessImpact should be a string. Got: ${businessImpact} (${typeof businessImpact})`,
+                )
+            }
+            this.businessImpact = businessImpact
+        }
+
+        if (isDef(impactLevel)) {
+            if (!inRange(impactLevel, config.impactLevel.min, config.impactLevel.max)) {
+                throw new TypeError(
+                    `impactLevel should be a number in range ${config.impactLevel.min} - ${config.impactLevel.max}. Got: ${impactLevel}`,
+                )
+            }
+            this.impactLevel = impactLevel
+        }
     }
 
     get metrics() {
