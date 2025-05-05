d3ed2ddb941cf8f4976b8cc5069c4d307b378c9f
Alex Ewerlöf <hanifbbz@gmail.com>
2025-05-05 18:58:34 +0200

    simplify the feedback blob

--- components/feedback-blob-component.js
+++ components/feedback-blob-component.js
@@ -1,24 +1,9 @@
 import { config } from '../config.js'
-import { icon } from '../lib/icons.js'
 
 export default {
     methods: {
-        icon,
-    },
-    props: {
-        caption: {
-            type: String,
-            default: 'Feedback',
-        },
-        prefill: String,
-    },
-    computed: {
-        href() {
-            const url = new URL(config.feedbackBlob.baseUrl)
-            if (this.prefill) {
-                url.searchParams.set('entry.879776814', this.prefill)
-            }
-            return url.toString()
+        openFeedback() {
+            globalThis.open(config.feedbackBlob.baseUrl, '_blank', 'noopener noreferrer')
         },
     },
 }
