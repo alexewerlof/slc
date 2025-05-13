2b9057436ad9658a85bd764e8829583f57dfa49c
Alex Ewerlöf <hanifbbz@gmail.com>
2025-05-13 17:19:21 +0200

    simplify component spec

--- components/index.js
+++ components/index.js
@@ -2,63 +2,71 @@ import { componentDefinition, getUrlStrs } from '../lib/component-loader.js'
 import { isStrLen } from '../lib/validation.js'
 
 const componentSpecifications = [
-    ' AJH  : ./assessment-component',
-    ' AJHC : ./alert/alert-chart-component',
-    ' AJH  : ./alert/alert-component',
-    ' AJHC : ./ui/announcement-component',
-    ' AJHC : ./axis-component',
-    ' AJHC : ./alert/burn-event-component',
-    ' AJHC : ./alert/burn-rate-component',
-    ' AJHC : ./calculator-component',
-    ' AJH  : ./chat-component',
-    ' AJHC : ./chat-thread-component',
-    ' AJHC : ./ui/code-block-component',
-    ' A HC : ./ui/book-cover-component',
-    ' AJH  : ./assessment-component-intro',
-    ' AJH  : ./condition-component', // TODO: needs a rewrite or remove
-    ' AJH  : ./consumer-component',
-    ' AJH  : ./consumption-component',
-    ' AJHC : ./ui/cookie-popup-component',
-    ' AJHC : ./ui/dialog-component',
-    ' AJHC : ./objective/error-budget-component',
-    ' AJH  : ./event-component', // TODO: needs a rewrite or remove
-    '  JH  : ./ext-link',
-    ' AJH  : ./failure-component',
-    ' AJH  : ./dependency-component',
-    ' AJHC : ./faq-component',
-    ' AJHC : ./feedback-blob-component',
-    ' AJHC : ./footer-component',
-    ' AJH  : ./formula-component',
-    ' AJHC : ./ui/header-component',
-    '  JHC : ./ui/help-component',
-    ' AJHC : ./ui/hero-component',
-    ' AJHC : ./assessment-graph-component',
-    ' AJHC : ./failure-sort-component',
-    ' AJH  : ./indicator/indicator-component',
-    ' AJHC : ./indicator/indicator-finder-component',
-    ' AJHC : ./ui/inline-select-component',
-    ' A HC : ./ui/loading-animation-component',
-    ' AJH  : ./ui/markdown-component',
-    ' AJH  : ./metric-component',
-    ' AJH  : ./objective/objective-component',
-    ' AJHC : ./percentage-overview-component',
-    ' AJHC : ./plot-2d-component',
-    ' AJH  : ./provider-component',
-    ' AJH  : ./service-component',
-    ' AJH  : ./service-metric-component', // TODO: needs a rewrite or remove
-    ' AJHC : ./ui/show-hide-component',
-    ' AJHC : ./ui/steps-component',
-    ' AJH  : ./summary-component',
-    ' AJHC : ./ui/tabs-component',
-    ' A HC : ./ui/tooltip-component',
-    ' AJH  : ./llm/llm-api-settings-component',
+    './assessment-component.jh&',
+    './alert/alert-chart-component.jhc&',
+    './alert/alert-component.jh&',
+    './ui/announcement-component.jhc&',
+    './axis-component.jhc&',
+    './alert/burn-event-component.jhc&',
+    './alert/burn-rate-component.jhc&',
+    './calculator-component.jhc&',
+    './chat-component.jh&',
+    './chat-thread-component.jhc&',
+    './ui/code-block-component.jhc&',
+    './ui/book-cover-component.hc&',
+    './assessment-component-intro.jh&',
+    // TODO: needs a rewrite or remove
+    './condition-component.jh&',
+    './consumer-component.jh&',
+    './consumption-component.jh&',
+    './ui/cookie-popup-component.jhc&',
+    './ui/dialog-component.jhc&',
+    './objective/error-budget-component.jhc&',
+    // TODO: needs a rewrite or remove
+    './event-component.jh&',
+    './ext-link.jh',
+    './failure-component.jh&',
+    './dependency-component.jh&',
+    './faq-component.jhc&',
+    './feedback-blob-component.jhc&',
+    './footer-component.jhc&',
+    './formula-component.jh&',
+    './ui/header-component.jhc&',
+    './ui/help-component.jhc',
+    './ui/hero-component.jhc&',
+    './assessment-graph-component.jhc&',
+    './failure-sort-component.jhc&',
+    './indicator/indicator-component.jh&',
+    './indicator/indicator-finder-component.jhc&',
+    './ui/inline-select-component.jhc&',
+    './ui/loading-animation-component.hc&',
+    './ui/markdown-component.jh&',
+    './metric-component.jh&',
+    './objective/objective-component.jh&',
+    './percentage-overview-component.jhc&',
+    './plot-2d-component.jhc&',
+    './provider-component.jh&',
+    './service-component.jh&',
+    // TODO: needs a rewrite or remove
+    './service-metric-component.jh&',
+    './ui/show-hide-component.jhc&',
+    './ui/steps-component.jhc&',
+    './summary-component.jh&',
+    './ui/tabs-component.jhc&',
+    './ui/tooltip-component.hc&',
+    './llm/llm-api-settings-component.jh&',
 ]
 
 function parseComponentSpec(componentSpec) {
-    const [flags, relativeUrlBase] = componentSpec.split(':').map((s) => s.trim())
-    if (!isStrLen(flags, 1)) {
-        throw new Error(`Invalid flags: ${flags}`)
+    const lastDot = componentSpec.lastIndexOf('.')
+    if (lastDot === -1) {
+        throw new Error(`Invalid componentSpec: ${componentSpec}`)
     }
+    const flags = componentSpec.slice(lastDot + 1).trim().toLowerCase()
+    if (flags.length <= 1) {
+        throw new SyntaxError(`Empty flags: ${componentSpec} => '${flags}'`)
+    }
+    const relativeUrlBase = componentSpec.slice(0, lastDot).trim()
     if (!isStrLen(relativeUrlBase, 5)) {
         throw new Error(`Invalid relativeUrlBase: ${relativeUrlBase}`)
     }
@@ -68,15 +76,13 @@ function parseComponentSpec(componentSpec) {
         throw new Error(`Invalid component name: ${name}`)
     }
 
-    const flagsUpperCase = flags.toUpperCase()
-
     return {
         name,
         relativeUrlBase,
-        isAsync: flagsUpperCase.includes('A'),
-        hasJs: flagsUpperCase.includes('J'),
-        hasHtml: flagsUpperCase.includes('H'),
-        hasCss: flagsUpperCase.includes('C'),
+        isAsync: flags.includes('&'),
+        hasJs: flags.includes('j'),
+        hasHtml: flags.includes('h'),
+        hasCss: flags.includes('c'),
     }
 }
 
