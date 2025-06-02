fe476151c8da5098df6d307f8ee5d9f77aefe005
Alex Ewerlöf <hanifbbz@gmail.com>
2025-06-02 09:36:36 +0200

    fix the manifest loading for _ui

--- app/_ui/index.js
+++ app/_ui/index.js
@@ -2,6 +2,9 @@ import { createApp } from '../../vendor/vue.js'
 import { config } from '../../config.js'
 import { showToast } from '../../lib/toast.js'
 import { registerAllComponents } from '../../components/index.js'
+import { loadJson } from '../../lib/share.js'
+
+const manifest = await loadJson('manifest.json')
 
 const app = createApp({
     data() {
@@ -28,6 +31,7 @@ const app = createApp({
         const stepLabels = ['Washing', 'Chopping', 'Cooking', 'Eating']
         const tabNames = ['Forms', 'Buttons', 'Typography', 'Layout']
         return {
+            manifest,
             // Expose the config to the UI
             config,
             inlineSelectOptions,
