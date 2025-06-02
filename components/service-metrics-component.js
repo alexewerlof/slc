8f1b65e8737886c920fdfb005e1cc27ce2ff660e
Alex Ewerlöf <hanifbbz@gmail.com>
2025-06-02 10:51:56 +0200

    modernize how prop type is defined in a couple of components

--- components/service-metrics-component.js
+++ components/service-metrics-component.js
@@ -1,4 +1,3 @@
-import { isInstance } from '../lib/validation.js'
 import { Service } from './service.js'
 import { config } from '../config.js'
 import { numL10n, percL10n } from '../lib/fmt.js'
@@ -12,8 +11,8 @@ export default {
     },
     props: {
         service: {
-            type: Object,
-            validator: (v) => isInstance(v, Service),
+            type: Service,
+            required: true,
         },
     },
     methods: {
