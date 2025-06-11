aa0b25193c8998abe6dfd7b6fa69ce29ff0a746c
Alex Ewerlöf <hanifbbz@gmail.com>
2025-06-11 11:56:57 +0200

    extract away the app name and descriptor building logic

--- index.js
+++ index.js
@@ -1,43 +1,15 @@
 import { createApp } from './vendor/vue.js'
 import { addUTM } from './lib/utm.js'
 import { registerAllComponents } from './components/index.js'
-import { isObj } from './lib/validation.js'
-import { loadJson } from './lib/share.js'
-
-const appNames = [
-    'uptime',
-    'assessment',
-    'calculator',
-    'templates',
-    'simulator',
-    'learn',
-    'chat',
-    'assess',
-]
+import { appDescriptors } from './app/index.js'
 
 // Attempting to load all manifests also serves as a quick smoke test
-const apps = await Promise.all(appNames.map(async (name) => {
-    const url = new URL(`./app/${name}/index.html`, globalThis.location.origin)
-    const manifestUrl = new URL(`./app/${name}/manifest.json`, globalThis.location.origin)
-    const manifest = await loadJson(manifestUrl)
-    if (!isObj(manifest)) {
-        throw new Error(`Manifest for ${name} is empty`)
-    }
-    return {
-        name,
-        url,
-        icon: (new URL(manifest.icons[0].src, manifestUrl)).toString(),
-        manifest,
-    }
-}))
-
-const calculatorApp = apps.find((app) => app.name === 'calculator')
+const apps = await appDescriptors()
 
 const app = createApp({
     data() {
         return {
             apps,
-            mainApp: calculatorApp,
         }
     },
     mounted() {
@@ -45,12 +17,13 @@ const app = createApp({
         // Previously the calculator was sitting at the root of the site.
         // If `urlVer` or `title` are present, redirect to the calculator
         if (url.searchParams.has('urlVer') || url.searchParams.has('title')) {
-            const { search } = url
+            const calculatorApp = apps.find((app) => app.name === 'calculator')
             const redirectUrl = new URL(calculatorApp.url)
+            const { search } = url
             redirectUrl.search = search
             globalThis.location.replace(addUTM(redirectUrl, {
                 source: 'web',
-                campaign: 'auto-redirect',
+                campaign: 'legacy-auto-redirect',
             }))
         }
     },
