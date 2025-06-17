5201a9743dca9a248ef1b25ec8533ec14b9463bf
Alex Ewerlöf <hanifbbz@gmail.com>
2025-06-17 21:30:48 +0200

    make it easier to copy the id with one click

--- components/ui/id-component.js
+++ components/ui/id-component.js
@@ -1,6 +1,19 @@
+import { showToast } from '../../lib/toast.js'
+
 export default {
     props: {
         entity: Object,
         required: true,
+    },
+    methods: {
+        async copy() {
+            try {
+                const textToCopy = this.$refs.copyNodeRef.innerText
+                await navigator.clipboard.writeText(textToCopy)
+                showToast('ID copied to clipboard!')
+            } catch (copyError) {
+                showToast(`Failed to copy text: ${copyError}`)
+            }
+        },
     },
 }
