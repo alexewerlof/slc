1f739937c67a085db1ae11f17cdea8007ae2a746
Alex Ewerlöf <hanifbbz@gmail.com>
2025-06-24 15:47:24 +0200

    move the prolog code generation to a separate file

---
+++ app/assessment/cmp/prolog.js
@@ -0,0 +1,183 @@
+import { Assessment } from '../../../components/assessment.js'
+import { isInstance } from '../../../lib/validation.js'
+
+function quoted(str) {
+    return `"${str.replace(/"/g, '\\"')}"`
+}
+
+export function assessment2prolog(assessment) {
+    if (!isInstance(assessment, Assessment)) {
+        throw new TypeError(`Expected an instance of assessment. Got ${assessment} (${typeof assessment})}`)
+    }
+
+    const lines = []
+
+    function fact(predicate, ...id) {
+        lines.push(`${predicate}(${id.join(', ')}).`)
+    }
+
+    fact(
+        '% consumer',
+        'ConsumerID',
+        'DisplayName',
+        'Description',
+        'Type',
+    )
+    assessment.consumers.forEach((consumer) => {
+        fact(
+            'consumer',
+            consumer.id,
+            quoted(consumer.displayName),
+            quoted(consumer.description),
+            consumer.type,
+        )
+    })
+
+    fact(
+        '% hasConsumption',
+        'ConsumerID',
+        'ConsumptionID',
+    )
+    assessment.consumptions.forEach((consumption) => {
+        fact(
+            'hasConsumption',
+            consumption.consumer.id,
+            consumption.id,
+        )
+    })
+
+    fact(
+        '% consumption',
+        'ConsumptionID',
+        'DisplayName',
+        'Description',
+    )
+    assessment.consumptions.forEach((consumption) => {
+        fact(
+            'consumption',
+            consumption.id,
+            quoted(consumption.displayName),
+            quoted(consumption.description),
+        )
+    })
+
+    fact(
+        '% dependency',
+        'ConsumptionID',
+        'ServiceID',
+    )
+
+    assessment.dependencies.forEach((dependency) => {
+        fact(
+            'dependency',
+            dependency.consumption.id,
+            dependency.service.id,
+        )
+    })
+
+    fact(
+        '% provider',
+        'ProviderID',
+        'DisplayName',
+        'Description',
+        'Type',
+    )
+    assessment.providers.forEach((provider) => {
+        fact(
+            'provider',
+            provider.id,
+            quoted(provider.displayName),
+            quoted(provider.description),
+            provider.type,
+        )
+    })
+
+    fact('% provides', 'ProviderID', 'ServiceID')
+    assessment.services.forEach((service) => {
+        lines.push(`provides(${service.provider.id}, ${service.id}).`)
+    })
+
+    fact(
+        '% service',
+        'ServiceID',
+        'DisplayName',
+        'Description',
+    )
+    assessment.services.forEach((service) => {
+        fact(
+            'service',
+            service.id,
+            quoted(service.displayName),
+            quoted(service.description),
+        )
+    })
+
+    fact(
+        '% measures',
+        'MetricID',
+        'ServiceID',
+    )
+
+    assessment.metrics.forEach((metric) => {
+        fact(
+            'measures',
+            metric.id,
+            metric.service.id,
+        )
+    })
+
+    fact(
+        '% metric',
+        'MetricID',
+        'DisplayName',
+        'Description',
+        'IsBoolean',
+        'NumericUnit',
+    )
+    assessment.metrics.forEach((metric) => {
+        fact(
+            'metric',
+            metric.id,
+            quoted(metric.displayName),
+            quoted(metric.description),
+            metric.isBoolean,
+            quoted(metric.numericUnit),
+        )
+    })
+
+    fact(
+        '% indicates',
+        'MetricID',
+        'FailureID',
+    )
+    assessment.metrics.forEach((metric) => {
+        for (const failure of metric.linkedFailures) {
+            fact(
+                'indicates',
+                metric.id,
+                failure.id,
+            )
+        }
+    })
+
+    fact(
+        '% failure',
+        'FailureID',
+        'Symptom',
+        'Consequence',
+        'BusinessImpact',
+        'ImpactLevel',
+    )
+    assessment.failures.forEach((failure) => {
+        fact(
+            'failure',
+            failure.id,
+            quoted(failure.symptom),
+            quoted(failure.consequence),
+            quoted(failure.businessImpact),
+            failure.impactLevel,
+        )
+    })
+
+    return lines.join('\n')
+}
