0e99f4ed6c45b4baacb573327443483846b8ec9d
Alex Ewerlöf <hanifbbz@gmail.com>
2025-07-26 13:13:52 +0200

    surgically move the main chunk of logic from chat-component.js to the new Agent.js

---
+++ components/llm/agent.js
@@ -0,0 +1,79 @@
+import { getFirstMessage, isToolsCallMessage } from './util.js'
+import { AssistantResponse, Thread, ToolCallsBead, ToolResultBead } from './thread.js'
+import { llm } from './llm.js'
+import { Toolbox } from './toolbox.js'
+import { isDef, isInstance } from '../../lib/validation.js'
+
+export class Agent {
+    /** Max consecutive tools calls */
+    static MAX_CALLS = 10
+
+    abortController = undefined
+
+    get isBusy() {
+        return this.abortController !== undefined
+    }
+
+    async completeThread(thread, tools) {
+        if (!isInstance(thread, Thread)) {
+            throw new TypeError(`Expected thread to be an instance of Thread. Got ${thread} (${typeof thread})`)
+        }
+
+        if (isDef(tools) && !isInstance(tools, Toolbox)) {
+            throw new TypeError(`Expected tools to be an instance of Toolbox. Got ${tools} (${typeof tools})`)
+        }
+
+        let consecutiveToolsCalls = 0
+        let lastMessageWasToolsCall = true
+        do {
+            const messages = await thread.toMessages()
+
+            const start = Date.now()
+
+            this.abortController = new AbortController()
+            const completion = await llm.getCompletion(messages, {
+                /*
+                    maxTokens: this.maxTokens,
+                    temperature: this.temperature,
+                    */
+                signal: this.abortController.signal,
+                tools: tools?.descriptor,
+            })
+            this.abortController = undefined
+
+            const message = getFirstMessage(completion)
+            const { usage } = completion
+            usage.duration = Date.now() - start
+
+            lastMessageWasToolsCall = tools && isToolsCallMessage(message)
+
+            if (!lastMessageWasToolsCall) {
+                const bead = new AssistantResponse(message.content)
+                bead.usage = usage
+                thread.add(bead)
+                return
+            }
+
+            const bead = new ToolCallsBead(message.tool_calls)
+            bead.usage = usage
+            thread.add(bead)
+            consecutiveToolsCalls++
+
+            if (consecutiveToolsCalls >= Agent.MAX_CALLS) {
+                throw new Error(`Stopping due to too many tool calls (max=${Agent.MAX_CALLS})`)
+            }
+
+            const toolResultMessages = await tools.exeToolCalls(message)
+            for (const toolResultMessage of toolResultMessages) {
+                thread.add(new ToolResultBead(toolResultMessage))
+            }
+        } while (lastMessageWasToolsCall)
+    }
+
+    abortCompletion(reason) {
+        if (this.abortController) {
+            this.abortController.abort(reason)
+            this.abortController = undefined
+        }
+    }
+}
--- components/llm/chat-component.js
+++ components/llm/chat-component.js
@@ -1,15 +1,14 @@
 import { config } from '../../config.js'
 import { showToast } from '../../lib/toast.js'
-import { getFirstMessage, isToolsCallMessage } from './util.js'
-import { AssistantResponse, ContentBead, Thread, ToolCallsBead, ToolResultBead, UserPromptBead } from './thread.js'
-import { llm } from './llm.js'
+import { ContentBead, Thread, UserPromptBead } from './thread.js'
 import { Toolbox } from './toolbox.js'
+import { Agent } from './agent.js'
 
 export default {
     data() {
         return {
             message: this.initiaPrompt,
-            abortController: undefined,
+            agent: new Agent(),
         }
     },
     props: {
@@ -35,7 +34,7 @@ export default {
             return config
         },
         isEditDisabled() {
-            return this.abortController !== undefined
+            return this.agent.isBusy
         },
         isMessageEmpty() {
             return this.message.trim() === ''
@@ -52,40 +51,7 @@ export default {
                     this.$refs.chatThreadComponent.scrollToBottom()
                 })
                 this.message = ''
-                const MAX_CONSECUTIVE_TOOLS_CALLS = 10
-                let consecutiveToolsCalls = 0
-                do {
-                    const messages = await this.thread.toMessages()
-                    this.abortController = new AbortController()
-                    const start = Date.now()
-                    const completion = await llm.getCompletion(messages, {
-                        maxTokens: this.maxTokens,
-                        temperature: this.temperature,
-                        signal: this.abortController.signal,
-                        tools: this.tools?.descriptor,
-                    })
-                    const duration = Date.now() - start
-                    const message = getFirstMessage(completion)
-                    const { usage } = completion
-                    usage.duration = duration
-                    if (!this.tools || !isToolsCallMessage(message)) {
-                        const bead = new AssistantResponse(message.content)
-                        bead.usage = usage
-                        this.thread.add(bead)
-                        break
-                    }
-                    const bead = new ToolCallsBead(message.tool_calls)
-                    bead.usage = usage
-                    this.thread.add(bead)
-                    consecutiveToolsCalls++
-                    if (consecutiveToolsCalls >= MAX_CONSECUTIVE_TOOLS_CALLS) {
-                        throw new Error(`Stopping due to too many tool calls (max=${MAX_CONSECUTIVE_TOOLS_CALLS})`)
-                    }
-                    const toolResultMessages = await this.tools.exeToolCalls(message)
-                    for (const toolResultMessage of toolResultMessages) {
-                        this.thread.add(new ToolResultBead(toolResultMessage))
-                    }
-                } while (true)
+                await this.agent.completeThread(this.thread, this.tools)
             } catch (error) {
                 this.thread.add(
                     new ContentBead({
@@ -103,14 +69,9 @@ export default {
                 this.$refs.chatThreadComponent.scrollToBottom()
                 this.$refs.promptInput.focus()
             })
-
-            this.abortController = undefined
         },
         abortCompletion(reason) {
-            if (this.abortController) {
-                this.abortController.abort(reason)
-                this.abortController = undefined
-            }
+            this.agent.abortCompletion(reason)
         },
     },
 }
