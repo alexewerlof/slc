ca16553bd4856c2ade3d55ae1b319016ebd1d68c
Alex Ewerlöf <hanifbbz@gmail.com>
2025-08-02 23:27:48 +0200

    keep a copy of the latest assessment state in storage to keep the changes when the page refreshes

--- app/assessment/index.js
+++ app/assessment/index.js
@@ -3,23 +3,51 @@ import { registerAllComponents } from '../../components/index.js'
 import { Assessment } from '../../components/assessment.js'
 import { currentUrlToState, loadJson } from '../../lib/share.js'
 import { registerComponents } from '../../lib/component-loader.js'
+import { Store } from '../../lib/store.js'
 
 const manifest = await loadJson('manifest.json')
 
 export const app = createApp({
     data() {
+        const store = new Store('assessment-current-state', true)
+
         const assessment = new Assessment()
 
-        try {
-            assessment.state = currentUrlToState()
-        } catch (e) {
-            console.warn('Using default because failed to load from URL:', e)
+        const url = new URL(globalThis.location.href)
+        if (url.searchParams.has('urlVer')) {
+            try {
+                assessment.state = currentUrlToState()
+            } catch (e) {
+                console.warn('Using default because failed to load from URL:', e)
+            }
         }
 
         return {
             manifest,
             assessment,
+            store,
         }
+    },
+    mounted() {
+        const url = new URL(globalThis.location.href)
+        if (url.searchParams.has('urlVer')) {
+            try {
+                assessment.state = currentUrlToState()
+            } catch (e) {
+                console.warn('Failed to load from URL:', e)
+            }
+        } else if (this.store.hasStoredValue) {
+            this.assessment.state = this.store.state
+        }
+    },
+    watch: {
+        assessment: {
+            handler() {
+                console.log('Assessment changed:', this.assessment.state)
+                this.store.state = this.assessment.state
+            },
+            deep: true,
+        },
     },
 })
 
