6b3beaa9ea10b3421e98907d301e06e9f74d02c3
Alex Ewerlöf <hanifbbz@gmail.com>
2025-08-09 11:27:42 +0200

    move the markdown manipulation code to a plugin

--- components/ui/markdown-component.js
+++ components/ui/markdown-component.js
@@ -1,20 +1,7 @@
 import { markdownIt } from '../../vendor/markdown-it.js'
+import linksTargetBlank from '../../lib/markdown.js'
 
-const md = markdownIt()
-
-// Remember old renderer, if overridden, or proxy to default renderer
-const defaultRender = md.renderer.rules.link_open ?? function (tokens, idx, options, env, self) {
-    return self.renderToken(tokens, idx, options)
-}
-
-md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
-    // Add target="_blank" and rel="noopener noreferrer" to all links
-    tokens[idx].attrSet('target', '_blank')
-    tokens[idx].attrSet('rel', 'noopener noreferrer')
-
-    // pass token to default renderer.
-    return defaultRender(tokens, idx, options, env, self)
-}
+const md = markdownIt().use(linksTargetBlank)
 
 export default {
     props: {
