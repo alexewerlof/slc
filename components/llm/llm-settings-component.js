9ae116bb816e36acf12d7bf740148cbf420aaeb2
Alex Ewerlöf <hanifbbz@gmail.com>
2025-08-11 12:52:13 +0200

    improve the UX for the clear log button on LLM Settings

--- components/llm/llm-settings-component.js
+++ components/llm/llm-settings-component.js
@@ -20,14 +20,12 @@ export default {
         log(...lines) {
             this.logLines.push(...lines)
         },
-        clearLog(skipConfirmation = false) {
-            if (skipConfirmation || confirm('Are you sure you want to clear the log?')) {
-                this.logLines.splice(0)
-            }
+        clearLog() {
+            this.logLines.splice(0)
         },
         async test() {
             try {
-                this.clearLog(true)
+                this.clearLog()
                 const testThread1 = new Thread()
                 const testKeyWord = 'wombat12'
                 this.log(`Test 1: Asking LLM to echo secret word...`)
