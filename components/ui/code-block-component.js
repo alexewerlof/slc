20be53c91117ed1fd184a75911ec91208bbac64c
Alex Ewerlöf <hanifbbz@gmail.com>
2025-12-17 20:33:32 +0100

    improve code block sharing

--- components/ui/code-block-component.js
+++ components/ui/code-block-component.js
@@ -1,4 +1,6 @@
+import { downloadFile, saveAs } from '../../lib/share.js'
 import { showToast } from '../../lib/toast.js'
+import { isFn } from '../../lib/validation.js'
 
 export default {
     props: {
@@ -7,15 +9,39 @@ export default {
             default: false,
         },
         title: String,
+        fileName: String,
+    },
+    computed: {
+        textContent() {
+            return this.$refs.codeRef.innerText
+        },
+        saveAsSupported() {
+            return isFn(window.showSaveFilePicker)
+        },
     },
     methods: {
         async copy() {
             try {
-                const textToCopy = this.$refs.codeRef.innerText
-                await navigator.clipboard.writeText(textToCopy)
+                await navigator.clipboard.writeText(this.textContent)
                 showToast('Copied to clipboard!')
             } catch (copyError) {
                 showToast(`Failed to copy text: ${copyError}`)
+            }
+        },
+        download() {
+            try {
+                downloadFile(this.textContent, 'data-1.txt')
+                showToast('Downloaded file!')
+            } catch (downloadError) {
+                showToast(`Failed to download file: ${downloadError}`)
+            }
+        },
+        async saveAs() {
+            try {
+                await saveAs(this.textContent, 'data-2')
+                showToast('Saved as file!')
+            } catch (saveError) {
+                showToast(`Failed to save as file: ${saveError}`)
             }
         },
     },
